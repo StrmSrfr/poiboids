360eaa1bb455c82ee45d1c3ce00637677a145c92
Samuel Edwin Ward <wardse@dshs.wa.gov>
2015-10-23 13:40:12 -0700

    first buggy draft of bees algorithm

--- poiboids.js
+++ poiboids.js
@@ -1,3 +1,34 @@
+function gravity(e1, e2) {
+    var G = 1,
+        d = e1.position.subtract(e2.position),
+        r = e1.position.distanceFrom(e2.position),
+        t = d.toUnitVector();
+
+    // TODO mass?
+    if (r > 0) {
+        return t.multiply(1/r*r);
+    } else {
+        return $V([0, 0]);
+    }
+}
+
+/**
+ * Takes a function:
+ * x.f(y) = z
+ * and returns a function so it may be called:
+ * g(x, y) = z
+ */
+function unmethodify(method) {
+    return function unmethodify$1() {
+        var rest = [],
+            i;
+        for (i = 1; i < arguments.length; i++) {
+            rest.push(arguments[i]);
+        }
+        method.call(arguments[0], rest);
+    }
+}
+
 /**
  * Constructor for boids.  Takes a jQuery object for the DOM element to
  * turn into a boid.
@@ -14,6 +45,32 @@ function Boid(el) {
     // TODO front, angular velocity?
     // TODO mass
 
+    this.step = function step() {
+        var forces = [],
+            i;
+        this.position =
+            this.position.add(this.velocity);
+        this.velocity =
+            this.velocity.add(this.acceleration);
+
+        forces = [];
+        for (i = 0; i < BOIDS.length; i++) {
+            forces.push(this.force(BOIDS[i]));
+        }
+
+        for (i = 0; i < forces.length; i++) {
+            this.acceleration =
+                this.acceleration.add(forces[i]);
+        }
+
+        this.$.css('left', this.position.e(1));
+        this.$.css('top', this.position.e(2));
+    };
+
+    this.force = function force(that) {
+        return gravity(this, that);
+    };
+
     return this;
 }
 
@@ -21,4 +78,10 @@ var BOIDS = [];
 
 $(function() {
     $('.content > *').css('position', 'relative').each(function () { BOIDS.push(new Boid(this)); });
+    window.setInterval(function () {
+        BOIDS.forEach(function (el) {
+            el.step();
+        });
+    },
+                      1000);
 });
