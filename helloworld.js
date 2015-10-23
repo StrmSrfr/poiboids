d5f9c218e21dbd9bf6294cc8c4725034187d512e
Samuel Edwin Ward <wardse@dshs.wa.gov>
2015-10-22 17:00:03 -0700

    adaptive UI

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,24 @@
-$(function () {
+function growHello(){
     var hello = $('.hello'),
         oldW = hello.width(),
         parentW = hello.parent().width(),
-        newW = 0.99 * parentW,
+        newW = 0.99 * Math.min(parentW, $(window).width()),
         size = parseInt(hello.css('font-size')),
         newSize = size * newW / oldW;
     hello.animate({ fontSize: newSize + 'px' }, 2000);
+}
+
+$(function () {
+    growHello();
+    $(window).resize($.debounce(100, growHello));
 });
+
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
